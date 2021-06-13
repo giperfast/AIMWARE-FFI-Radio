8d76fbe71800ae1ce774d230578d932d7d0a52d2
giperfast <33413844+giperfast@users.noreply.github.com>
2021-06-13 13:56:08 +0500

    Update Radio.lua
    Added display of the stream name in the console or on the screen.

--- Radio.lua
+++ Radio.lua
@@ -38,21 +38,64 @@ local BASS_StreamCreateURL = ffi.cast("unsigned long(__stdcall*)(const char *, u
 local BASS_ChannelSetAttribute = ffi.cast("int(__stdcall*)( unsigned long, unsigned long, float )", ffi.C.GetProcAddress(bass_dll, "BASS_ChannelSetAttribute"));
 local BASS_ChannelPlay = ffi.cast("int( __stdcall*)(unsigned long, bool)", ffi.C.GetProcAddress(bass_dll, "BASS_ChannelPlay"));
 local BASS_ChannelStop = ffi.cast("int(__stdcall*)(unsigned long)", ffi.C.GetProcAddress(bass_dll, "BASS_ChannelStop"));
+local BASS_ChannelGetTags = ffi.cast("char*(__stdcall*)(unsigned long, unsigned long )", ffi.C.GetProcAddress(bass_dll, "BASS_ChannelGetTags"))
 
-local BASS_DEVICE_STEREO, BASS_ATTRIB_VOL, stream, playing, station_backup, stream_backup = 0x8000, 2, 0, false, nil, nil;
+local BASS_DEVICE_STEREO, BASS_ATTRIB_VOL, BASS_TAG_META, stream, playing, station_backup, stream_backup, metadata_backup, state, fade = 0x8000, 2, 5, 0, false, nil, nil, nil, 0, 0;
 BASS_Init(-1, 44100, BASS_DEVICE_STEREO, nil, nil);
+
+local w,h = draw.GetScreenSize();
+local w = w * 0.5;
+local font = draw.CreateFont("Bahnschrift", 14)
+local curtime = 0;
 
 local names = {};
 for _, station in ipairs( stations ) do
     table.insert( names, station.name );
 end;
 
-local w,h = draw.GetScreenSize();
+local ref = gui.Tab(gui.Reference('Settings'), 'r.tab', 'Radio Settings');
+local ref_box = gui.Groupbox( ref, "Radio Settings", 15, 15, 300, 500 );
+local multibox = gui.Multibox( ref_box, 'Stream Title');
+local render_enable = gui.Checkbox( multibox, 'r.render.enable', 'Render Stream Title', true );
+local print_enable = gui.Checkbox( multibox, 'r.print.enable', 'Print Stream Title', true );
+multibox:SetDescription( 'Displays the stream name on the screen and in the console.' );
+
 local window = gui.Window( 'r.window', 'Radio', 25, (h/2)-117.5, 212, 235 );
 local box = gui.Groupbox( window, "General", 10, 10, 192, 0 );
 local radio_play = gui.Checkbox( box, 'r.enable', 'Enable radio', true );
 local radio_station = gui.Combobox( box, 'r.station', 'Station', unpack(names) );
 local radio_volume = gui.Slider( box, 'r.volume', 'Volume', 10, 0, 100 );
+
+local function TitleRenderHandler(Title)
+    if state == 0 then
+        if fade == 0 and print_enable:GetValue() then
+            print(Title);
+        end
+        fade = fade + 1;
+        if fade >= 255 then
+            state = 1;
+        end;
+    elseif state == 1 then
+        if curtime == 0 then curtime = globals.CurTime() end;
+        if (globals.CurTime() - curtime > 10) then
+            fade = fade - 1;
+            if fade <= 0 then
+                state = 0;
+                metadata_backup = metadata;
+                curtime = globals.CurTime();
+            end;
+        end;
+    end;
+    if (render_enable:GetValue()) then
+        draw.SetFont(font);
+        draw.Color(225,225,225,fade);
+        draw.Text(w-draw.GetTextSize(Title)/2, h-27, Title);
+    else
+        state = 0;
+        fade = 0;
+        metadata_backup = metadata;
+    end
+end
 
 local function RadioHandler()
     local station = radio_station:GetValue();
@@ -81,6 +124,19 @@ local function RadioHandler()
 
     if (playing and stream ~= 0) then
         BASS_ChannelSetAttribute(stream, BASS_ATTRIB_VOL, radio_volume:GetValue()/100);
+        if render_enable:GetValue() or print_enable:GetValue() then
+            metadata = BASS_ChannelGetTags(stream, BASS_TAG_META);
+            if metadata ~= nil then
+                metadata = ffi.string(metadata);
+                if metadata_backup ~= metadata then
+                    Title = metadata:match("StreamTitle='([%w%s%p]+)'");
+                    if Title ~= nil then
+                        if string.find(Title, "';StreamUrl='") then Title = string.gsub(Title, "';StreamUrl='", "") end;
+                        TitleRenderHandler(Title);
+                    end;
+                end;
+            end;
+        end;
     end;
 end;
 
@@ -100,5 +156,4 @@ end;
 callbacks.Register("Draw", "Radio", RadioHandler);
 callbacks.Register("Draw", "GUI", GUIHandler);
 callbacks.Register("Unload", UnloadHandler);
-
 
